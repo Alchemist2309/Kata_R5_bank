011cb412ac9ab6246859db6c594300ced9959e40
Sergio_Vargas <sergio.vargas@grupor5.com>
2023-06-02 17:25:59 -0500

    add validations in fucntions

--- src/Kata_2.py
+++ src/Kata_2.py
@@ -6,9 +6,9 @@ transacciones = []
 def deposit():
     while True:
         consulta = input("¿Desea ingresar un depósito? (S/N): ")
-        if not consulta.isalpha():
+        if not consulta.isalpha() or consulta.upper() != 'S' and consulta.upper() != 'N':
             print("El valor ingresado no es correcto")
-        elif consulta.lower() == 'n':
+        if consulta.upper() == 'N':
             break
         elif consulta.lower() == 's':
             monto = float(input("Ingrese monto a depositar: "))
@@ -18,6 +18,7 @@ def deposit():
                 date = datetime.datetime.now().strftime("%d/%m/%Y")
                 transacciones.append((date, monto))
                 print(f"Se ha depositado {monto} correctamente.")
+    print("el valor de la lista es: ", transacciones)
     withdraw()
 
 
@@ -25,15 +26,15 @@ def deposit():
 def withdraw():
     while True:
         consulta_retiro = input("¿Desea hacer un retiro? (s/n): ")
-        if not consulta_retiro.isalpha():
+        if not consulta_retiro.isalpha() or consulta_retiro.upper() != 'S' and consulta_retiro.upper() != 'N':
             print("El valor ingresado no es correcto")
         if consulta_retiro.lower() == 'n':
             print_statement()
             break
         elif consulta_retiro.lower() == 's':
-            retiro = float(input("Ingrese la cantidad del retiro"))
-            if retiro <= 0:
-                print("Digito un valor incorrecto o no tiene fondos suficiente:")
+            retiro = float(input("Ingrese la cantidad del retiro: "))
+            if retiro <= 0 or retiro > sum([monto for date, monto in transacciones]):
+                print("Digito un valor incorrecto o no tiene fondos suficiente: ")
             else:
                 date = datetime.datetime.now().strftime("%d/%m/%Y")
                 transacciones.append((date, - retiro))
